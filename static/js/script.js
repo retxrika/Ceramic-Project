6c378ffb197b568c8ee75340d821a97962fde30f
kensoi <65781959+kensoi@users.noreply.github.com>
2022-07-21 20:40:38 +0500

    stash

--- static/js/script.js
+++ static/js/script.js
@@ -1,34 +1,40 @@
 $(document).ready(function() {
-    let position = 0;
-    let c = 0
-    const slidesToShow = 1;
-    const slidesToScroll = 1;
-    const container = $('.slider-container');
-    const track = $('.slider-track');
-    const item = $('.slider-item');
-    const itemsCount = item.length;
-    const itemWidth = container.width() / slidesToShow;
-    const movePosition = slidesToScroll * itemWidth;
-    let timerId = setInterval(() => Next(), 6000);
-    item.each(function (index, item) {
-        $(item).css ({
-            minWidth: itemWidth,
-        });
-    });
-    const setPosition = () => {
-        track.css({
-            transform: `translateX(${position}px)`
-        });
-    };
-    const Prev = () => {
-        position += movePosition * itemsCount; 
-        setPosition();
-    };
-    const Next = () => {
-        if (position <= (-(itemsCount - slidesToShow) * itemWidth)) {
-            Prev();
-          }
-        position -= movePosition; 
-        setPosition();
-    };
+    const slider = new ChiefSlider('.slider', {
+        loop: true,
+        autoplay: true,
+        interval: 5000, // интервал перелистывания
+      });
+
+    // let position = 0;
+    // let c = 0
+    // const slidesToShow = 1;
+    // const slidesToScroll = 1;
+    // const container = $('.slider-container');
+    // const track = $('.slider-track');
+    // const item = $('.slider-item');
+    // const itemsCount = item.length;
+    // const itemWidth = container.width() / slidesToShow;
+    // const movePosition = slidesToScroll * itemWidth;
+    // let timerId = setInterval(() => Next(), 6000);
+    // item.each(function (index, item) {
+    //     $(item).css ({
+    //         minWidth: itemWidth,
+    //     });
+    // });
+    // const setPosition = () => {
+    //     track.css({
+    //         transform: `translateX(${position}px)`
+    //     });
+    // };
+    // const Prev = () => {
+    //     position += movePosition * itemsCount; 
+    //     setPosition();
+    // };
+    // const Next = () => {
+    //     if (position <= (-(itemsCount - slidesToShow) * itemWidth)) {
+    //         Prev();
+    //       }
+    //     position -= movePosition; 
+    //     setPosition();
+    // };
 });
